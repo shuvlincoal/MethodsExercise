67329ee2998a66a4a939ca907fbd411127853b5d
mangosax <mcguerty@att.net>
2023-08-24 14:44:00 -0500

    methods exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?: ");
+            string favColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?: ");
+            string favAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?: ");
+            string favBand = Console.ReadLine();
+
+            Console.WriteLine($"{userName} dyes his hair {favColor}");
+            Console.WriteLine($" while riding a {favAnimal} listening");
+            Console.WriteLine($"to {favBand}");
         }
     }
 }
