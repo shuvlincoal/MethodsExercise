d9aa74828908effbc67c30649793033186430d3f
mangosax <mcguerty@att.net>
2023-08-24 15:23:25 -0500

    methods exercise 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,6 +2,38 @@
 {
     public class Program
     {
+        //public static int Add(int nbr1, int nbr2)
+        //{
+        //    int sumNbrs = nbr1 + nbr2;
+        //    return sumNbrs;
+        //}
+
+        //public static int Multiply(int nbr1, int nbr2)
+        //{
+        //    int prodNbrs = nbr1 * nbr2;
+        //    return prodNbrs;
+        //}
+        public static int Add(params int[] numbers)
+        {
+            int sumNbrs = 0;
+            foreach (int num in numbers)
+            {
+                sumNbrs = sumNbrs + num;
+            }
+            return sumNbrs;
+        }
+
+        public static int Multiply(params int[] numbers)
+        {
+            int prodNbrs = 1;
+            foreach (int num in numbers)
+            {
+                prodNbrs = prodNbrs * num;
+            }
+            return prodNbrs;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("What is your name?: ");
@@ -19,6 +51,16 @@
             Console.WriteLine($"{userName} dyes his hair {favColor}");
             Console.WriteLine($" while riding a {favAnimal} listening");
             Console.WriteLine($"to {favBand}");
+
+            int blahInt = Program.Add(4, 5);
+            Console.WriteLine($"Sum of nbr1 and nbr2 is {blahInt}");
+
+            int fooInt = Program.Multiply(4, 5);
+            Console.WriteLine($"Product of nbr1 and nbr2 is {fooInt}");
+
         }
+
+
+
     }
 }
